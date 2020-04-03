dd25fcf4c3db5b3b54bb967fd20cf3cfdf554d7c
XnilinX <linx1984@hotmail.it>
2020-04-03 12:34:56 +0200

    Initial commit

---
+++ src/app/ricoverati-list/ricoverati-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ricoverati-list',
+  templateUrl: './ricoverati-list.component.html',
+  styleUrls: ['./ricoverati-list.component.css']
+})
+export class RicoveratiListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
